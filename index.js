880d19ae757de9011135cd0d894511d7c043a47f
jjeejj <wenjunjiang1993@163.com>
2016-09-28 10:53:50 +0800

    逐行处理文件

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 var fs = require('fs');
 
-const devicemac_one = 'FCAD0F0647F0.....';
-const devicemac_two = '27134225';
+const devicemac_one = 'FCAD0F0647F0';
+const devicemac_two = '202818A1DBA2';
+var isDelete = true;
 
+//合并为csv文件------简单的读取写入，没有利用流
 
-
-var files = fs.readdir('./csv',function (err,files) {
+var files = fs.readdir('./csv/2016-09-27',function (err,files) {
 	if(err){
 		console.log(err);
 		return false;
 	}
-
+	console.log(files.length);
+	//开始合并
 	for(var i = 0; i < files.length; i++){
 		var filename = files[i];
-		var data = fs.readFileSync('./csv/'+filename,{encoding:'utf8'}) 
+		var data = fs.readFileSync('./csv/2016-09-27/'+filename,{encoding:'utf8'}) 
 			
 		//分设备mac 进行分开合并
 		if(filename.indexOf(devicemac_one) >= 0){
-			fs.appendFileSync('./csv/'+devicemac_one+'.csv',data);
+			fs.appendFileSync('./csv/2016-09-27/'+devicemac_one+'.csv',data);
 
 		}else if(filename.indexOf(devicemac_two) >=0){
-			console.log(i);
-			fs.appendFileSync('./csv/'+devicemac_two+'.csv',data);
-		}			
+			fs.appendFileSync('./csv/2016-09-27/'+devicemac_two+'.csv',data);
+		}
+
+		if(isDelete){
+			fs.unlinkSync('./csv/2016-09-27/'+filename); //删除原始文件		
+		}
 	}
+	//结束合并
 })
